578cf5f709b4236ec6b225bbcae4e6991e5932f3
Ivan Gonzalez <gohanckz@protonmail.com>
2020-11-10 11:33:41 -0300

    Update guillotine.py
    Update

--- guillotine.py
+++ guillotine.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #_*_ coding: utf8 _*_
 
 #------------------------------------------------------------
@@ -7,7 +7,7 @@
 # ----            Gohanckz - W0lf_F4ng                  ----|
 # ----            Contact : gohanckz@gmail.cl           ----|
 # ----            Contact : ms@w0lff4ng.org             ----|
-# ----            Version : 1.0                         ----|
+# ----            Version : 1.0.1                       ----|
 #------------------------------------------------------------
 
 
@@ -15,14 +15,24 @@ try:
     import requests
     import argparse
     from banner import banner
+    from prettytable import PrettyTable
     import termcolor as t
+    from textwrap import fill
+    import sys
 except ImportError as err:
     print("Some libraries are missing:")
     print(err)
     
 parser = argparse.ArgumentParser(description="GUILLOTINE")
-parser.add_argument('-t','--target',help="example: python guillotine.py -t https://www.domain.com")
+parser.add_argument('-t','--target',help="example: python -t https://www.domain.com")
+parser.add_argument('-v','--verbose',action='store_true',help="show all information from headers")
+parser.add_argument('-o','--output',help="save report in a file.")
 parser = parser.parse_args()
+
+
+
+
+
 
 # Security headers
 security_headers =['Strict-Transport-Security',
@@ -41,42 +51,72 @@ security_headers =['Strict-Transport-Security',
                     'Upgrate-Insecure-Requests',
                     'X-Powered-By']
 
+table = PrettyTable()
+table_info = PrettyTable()
+
+column_names = ["         HEADER         ", "             INFORMATION               "]
+column_names_report = ["   ENABLED SECURITY HEADERS   ","     MISSING SECURITY HEADERS     "]
 
 def main():
-    banner()
     headers = []
     enabled_headers = []
+    info_headers = []
     if parser.target:
-        try:
-            url = requests.get(url=parser.target)
-            hdrs = dict(url.headers)
-            for i in hdrs:
-                headers.append(i)
-            missing_headers = set(security_headers) - set(headers)
-            for k in hdrs:
-                if k in security_headers:
-                    enabled_headers.append(k)
-            
-            print("+------------------------------------------+")
-            print("|          ENABLED SECURITY HEADERS        |")
-            print("+------------------------------------------+")
-            for i in enabled_headers:
-                print("|_"+t.colored(" [+] ","green")+ i+" "*(35-len(i))+"_|")
-            print("+------------------------------------------+")
-            print("|          MISSING SECURITY HEADERS        |")
-            print("+------------------------------------------+")
-            for i in missing_headers:
-                print("|_"+t.colored(" [x] ","red")+ i+" "*(35-len(i))+"_|")
-            print("+------------------------------------------+")
-        except:
-            print("connection cannot be established...\nusage: python guillotine.py -t https://www.domain.com")
-       
+        url = requests.get(url=parser.target)
+        hdrs = dict(url.headers)
+
+        for i in hdrs:
+            headers.append(i)
+            info_headers.append(hdrs[i])
+
+        missing_headers = [] 
+        for sh in security_headers:
+            if not sh.lower() in [h.lower() for h in headers]:
+                missing_headers.append(sh)
+
+        for k in hdrs:
+            if k.lower() in [sh.lower() for sh in security_headers]:
+                enabled_headers.append(k)
+
+        while len(enabled_headers) < len(missing_headers):
+            enabled_headers.append(" ")
+
+        while len(enabled_headers) > len(missing_headers):
+            missing_headers.append(" ")
+        
+        if parser.verbose:
+            banner()
+            table_info.add_column(column_names_report[0],enabled_headers)
+            table_info.add_column(column_names_report[1],missing_headers)
+            table_info.align="l"
+            print(table_info)
+            table.add_column(column_names[0],headers)
+            table.add_column(column_names[1],list(map(lambda text: fill(text,width = 46),info_headers)))
+            table.align="l"
+            print(table)                                            
+        else:
+            banner()
+            table_info.add_column(column_names_report[0],enabled_headers)
+            table_info.add_column(column_names_report[1],missing_headers)
+            table_info.align="l"
+            print(table_info)
+
+        if parser.output:
+            path = parser.output
+            with open(path,'w') as f: 
+                if parser.verbose:
+                    f.write(str(table_info))
+                    f.write('\n')
+                    f.write(str(table))
+                else:
+                    f.write(str(table_info))
+            f.close() 
+        
     else:
-        print("you must set a target...\nusage: python guillotine.py -t https://www.domain.com")
+        print("connection can't be established...")
+        print("type python3 guillotine.py -h to show more options")
        
 
 
 if __name__=='__main__':
     main()
-
-    
