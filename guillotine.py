373e0f6c911ce03556182da069eb5c04e053f2da
Ivan Gonzalez <gohanckz@protonmail.com>
2022-09-25 11:29:07 -0300

    Update guillotine.py
    The programming logic was changed and the security headers were updated according to owasp.

--- guillotine.py
+++ guillotine.py
@@ -3,120 +3,109 @@
 
 #------------------------------------------------------------
 #-----            GUILLOTINE                           -----|
-# ----            SECURITY HEADERS HTTP FINDER          ----|
-# ----            Gohanckz - W0lf_F4ng                  ----|
-# ----            Contact : gohanckz@gmail.cl           ----|
-# ----            Contact : ms@w0lff4ng.org             ----|
-# ----            Version : 1.0.1                       ----|
+# ----            FINDER HTTP SECURITY HEADERS          ----|
+# ----            Gohanckz                              ----|
+# ----            Contact : igonzalez@pwnsec.cl         ----|
+# ----            Version : 2.0                         ----|
 #------------------------------------------------------------
-
-
 try:
-    import requests
-    import argparse
     from banner import banner
     from prettytable import PrettyTable
-    import termcolor as t
-    from textwrap import fill
-    import sys
+    import requests
+    import argparse
 except ImportError as err:
     print("Some libraries are missing:")
     print(err)
-    
-parser = argparse.ArgumentParser(description="GUILLOTINE")
-parser.add_argument('-t','--target',help="example: python -t https://www.domain.com")
-parser.add_argument('-v','--verbose',action='store_true',help="show all information from headers")
-parser.add_argument('-o','--output',help="save report in a file.")
+
+parser = argparse.ArgumentParser(description="Finder Security Headers")
+parser.add_argument("-t","--target",help="Show http security headers enabled and missing")
+parser.add_argument("-v","--verbose",action="store_true",help="Show full response")
 parser = parser.parse_args()
 
+try:
+    url = requests.get(url=parser.target)    
 
 
+    security_headers = [
+        "Strict-Transport-Security",
+        "X-Frame-Options",
+        "X-Content-Type-Options",
+        "Content-Security-Policy",
+        "X-Permitted-Cross-Domain-Policies",
+        "Referrer-Policy",
+        "Clear-Site-Data",
+        "Cross-Origin-Embedder-Policy",
+        "Cross-Origin-Opener-Policy",
+        "Cross-Origin-Resource-Policy",
+        "Cache-Control"
+    ]
 
+    info_headers = []
+    headers_site = []
+    security_headers_site = []
+    missing_headers = []
 
+    headers = dict(url.headers)
 
-# Security headers
-security_headers =['Strict-Transport-Security',
-                    'X-XSS-Protection',
-                    'X-Content-Type-Options',
-                    'X-Frame-Options',
-                    'Content-Security-Policy',
-                    'Public-Key-Pins',
-                    'X-Permitted-Cross-Domain',
-                    'Referrer-Policy',
-                    'Expect-CT',
-                    'Feature-Policy',
-                    'Content-Security-Policy-Report-Only',
-                    'Expect-CT',
-                    'Public-Key-Pins-Report-Only',
-                    'Upgrate-Insecure-Requests',
-                    'X-Powered-By']
+    for i in headers:
+        headers_site.append(i)
 
-table = PrettyTable()
-table_info = PrettyTable()
+    for i in headers:
+        info_headers.append(headers[i])
 
-column_names = ["         HEADER         ", "             INFORMATION               "]
-column_names_report = ["   ENABLED SECURITY HEADERS   ","     MISSING SECURITY HEADERS     "]
+    for i in headers_site: 
+        if i in security_headers:
+            security_headers_site.append(i)
+        
+    for j in security_headers:
+        if not j in [h for h in headers_site]:
+            missing_headers.append(j)
+
+    table = PrettyTable()
+    table.add_column("Header",headers_site)
+    table.add_column("Information",info_headers)
+    table.align="l"
+
+    while len(security_headers_site) < len(missing_headers):
+        security_headers_site.append(" ")
+
+    while len(security_headers_site) > len(missing_headers):
+        missing_headers.append(" ")
+
+    count = 0
+    for i in security_headers_site:
+        if i != " ":
+            count += 1
+
+    s_table = PrettyTable()
+    s_table.add_column("Enabled Security Header",security_headers_site)
+    s_table.add_column("Missing Security Header",missing_headers)
+    s_table.align="l"
+except:
+    print("[!] time out, unable to connect to site.")
 
 def main():
-    headers = []
-    enabled_headers = []
-    info_headers = []
-    if parser.target:
-        url = requests.get(url=parser.target)
-        hdrs = dict(url.headers)
-
-        for i in hdrs:
-            headers.append(i)
-            info_headers.append(hdrs[i])
-
-        missing_headers = [] 
-        for sh in security_headers:
-            if not sh.lower() in [h.lower() for h in headers]:
-                missing_headers.append(sh)
-
-        for k in hdrs:
-            if k.lower() in [sh.lower() for sh in security_headers]:
-                enabled_headers.append(k)
-
-        while len(enabled_headers) < len(missing_headers):
-            enabled_headers.append(" ")
-
-        while len(enabled_headers) > len(missing_headers):
-            missing_headers.append(" ")
-        
-        if parser.verbose:
-            banner()
-            table_info.add_column(column_names_report[0],enabled_headers)
-            table_info.add_column(column_names_report[1],missing_headers)
-            table_info.align="l"
-            print(table_info)
-            table.add_column(column_names[0],headers)
-            table.add_column(column_names[1],list(map(lambda text: fill(text,width = 46),info_headers)))
-            table.align="l"
-            print(table)                                            
-        else:
-            banner()
-            table_info.add_column(column_names_report[0],enabled_headers)
-            table_info.add_column(column_names_report[1],missing_headers)
-            table_info.align="l"
-            print(table_info)
-
-        if parser.output:
-            path = parser.output
-            with open(path,'w') as f: 
-                if parser.verbose:
-                    f.write(str(table_info))
-                    f.write('\n')
-                    f.write(str(table))
-                else:
-                    f.write(str(table_info))
-            f.close() 
-        
-    else:
-        print("connection can't be established...")
-        print("type python3 guillotine.py -h to show more options")
-       
-
-
-if __name__=='__main__':
+    banner()
+    try:
+        print("\n[*] Analyzing target : ",parser.target)
+        print("[*] Security headers enabled :", count)
+        print("[*] Missing Security Headers :",len(missing_headers))
+    except:
+        print("[!] Syntax Error.")
+        print("[+] Usage: python3 guillotine.py -t http://examplse.site")
+def target():
+    try:      
+        print(s_table)
+    except:
+        pass
+def verbose():
+    try:
+        print(table)
+    except:
+        pass
+if __name__ == '__main__':
     main()
+    if parser.verbose:
+        verbose()
+    elif parser.target:
+        target()
