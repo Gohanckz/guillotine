caee2c28cf4ada43d888374c171be66ac065efd9
Ivan Gonzalez <gohanckz@gmail.com>
2023-11-22 12:07:20 -0300

    Update guillotine.py

--- guillotine.py
+++ guillotine.py
@@ -3,10 +3,10 @@
 
 #------------------------------------------------------------
 #-----            GUILLOTINE                           -----|
-# ----            FINDER HTTP SECURITY HEADERS          ----|
-# ----            Gohanckz                              ----|
-# ----            Contact : igonzalez@pwnsec.cl         ----|
-# ----            Version : 2.0                         ----|
+#----             FINDER HTTP SECURITY HEADERS          ----|
+#----             Gohanckz                              ----|
+#----             Contact : gohanckz@gmail.com          ----|
+#----             Version : 2.2                         ----|
 #------------------------------------------------------------
 try:
     from banner import banner
@@ -14,51 +14,75 @@ try:
     import requests
     import argparse
     from urllib3.exceptions import InsecureRequestWarning
+    from requests.auth import HTTPBasicAuth
+    from requests_ntlm import HttpNtlmAuth
 except ImportError as err:
     print("Some libraries are missing:")
     print(err)
 
 security_headers = [
-    "Strict-Transport-Security",
-    "X-Frame-Options",
-    "X-Content-Type-Options",
-    "Content-Security-Policy",
-    "X-Permitted-Cross-Domain-Policies",
-    "Referrer-Policy",
-    "Clear-Site-Data",
-    "Cross-Origin-Embedder-Policy",
-    "Cross-Origin-Opener-Policy",
-    "Cross-Origin-Resource-Policy",
-    "Cache-Control"
+    "Strict-Transport-Security".lower(),
+    "X-Frame-Options".lower(),
+    "X-Content-Type-Options".lower(),
+    "Content-Security-Policy".lower(),
+    "X-Permitted-Cross-Domain-Policies".lower(),
+    "Referrer-Policy".lower(),
+    "Clear-Site-Data".lower(),
+    "Cross-Origin-Embedder-Policy".lower(),
+    "Cross-Origin-Opener-Policy".lower(),
+    "Cross-Origin-Resource-Policy".lower(),
+    "Cache-Control".lower(),
 ]
 
 recommended_versions = {
-    "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
-    "Content-Security-Policy": "default-src 'self';",
-    "X-Content-Type-Options": "nosniff",
-    "X-Frame-Options": "SAMEORIGIN",
-    "X-XSS-Protection": "1; mode=block",
-    "Referrer-Policy": "no-referrer-when-downgrade",
-    "Feature-Policy": "vibrate 'none'; geolocation 'none';",
-    "Permissions-Policy": "geolocation=(), microphone=()",
-    "Expect-CT": "max-age=86400, enforce"
+    "Strict-Transport-Security".lower(): "max-age=31536000; includeSubDomains",
+    "X-Frame-Options".lower(): "SAMEORIGIN",
+    "X-Content-Type-Options".lower(): "nosniff",
+    "Content-Security-Policy".lower(): "default-src 'self';",
+    "X-Permitted-Cross-Domain-Policies".lower(): "none",
+    "Referrer-Policy".lower(): "no-referrer-when-downgrade",
+    "Clear-Site-Data".lower(): '"cache", "cookies", "storage", "executionContexts"',
+    "Cross-Origin-Embedder-Policy".lower(): "require-corp",
+    "Cross-Origin-Opener-Policy".lower(): "same-origin",
+    "Cross-Origin-Resource-Policy".lower(): "same-origin",
+    "Cache-Control".lower(): "no-cache, no-store, must-revalidate",
+    "X-XSS-Protection".lower(): "1; mode=block",
+    "Feature-Policy".lower(): "vibrate 'none'; geolocation 'none';",
+    "Permissions-Policy".lower(): "geolocation=(), microphone=()",
+    "Expect-CT".lower(): "max-age=86400, enforce"
 }
 
-def check_security_header_versions(headers):
+def check_security_header_versions(headers, parser):
     outdated_headers = {}
     for header, value in headers.items():
-        if header in recommended_versions and value != recommended_versions[header]:
-            outdated_headers[header] = value
+        if (
+                header.lower() in recommended_versions
+            ) and (
+                value.lower() != recommended_versions[header.lower()].lower()
+                ):
+            outdated_headers[header] = (value[:38]+"..." if (len(value) > 40 and not parser.verbose) else value)
     return outdated_headers
 
 parser = argparse.ArgumentParser(description="Finder Security Headers")
-parser.add_argument("-t","--target",help="Show http security headers enabled and missing")
+parser.add_argument("-t","--target",help="Show http security headers enabled and missing", required=True)
+parser.add_argument("--compare-versions",action="store_true",help="Show the recomended version for headers in use.")
+parser.add_argument("--ntlm", help="Use NTLM Authentication. Format: [<domain>\\\\]<username>:<password>")
+parser.add_argument("--basic", help="Use BASIC Authentication. Format: <username>:<password>")
 parser.add_argument("-v","--verbose",action="store_true",help="Show full response")
 parser = parser.parse_args()
 
 try:
     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-    url = requests.get(url=parser.target, verify=False)
+    
+    auth = None
+    if( parser.basic ):
+        basicAuth = parser.basic.split(":")
+        auth = HTTPBasicAuth(basicAuth[0], "".join(basicAuth[1:]))
+    elif( parser.ntlm ):
+        ntlmAuth = parser.ntlm.split(":")
+        auth = HttpNtlmAuth(ntlmAuth[0], "".join(ntlmAuth[1:]))
+
+    url = requests.get(url=parser.target, verify=False, auth=auth)
 
     info_headers = []
     headers_site = []
@@ -74,12 +98,12 @@ try:
         info_headers.append(headers[i])
 
     for i in headers_site: 
-        if i in security_headers:
+        if i.lower() in security_headers:
             security_headers_site.append(i)
         
     for j in security_headers:
-        if not j in [h for h in headers_site]:
-            missing_headers.append(j)
+        if not j.lower() in [h.lower() for h in headers_site]:
+            missing_headers.append( "-".join( [ word.capitalize() for word in j.split("-") ] ) )
 
     table = PrettyTable()
     table.add_column("Header",headers_site)
@@ -131,11 +155,15 @@ def verbose():
 
 if __name__ == '__main__':
     main()
-    outdated_headers = check_security_header_versions(headers)
-    if outdated_headers:
-        print("\n[!] The following headers are outdated:")
-        for header, value in outdated_headers.items():
-            print(f"  - {header}: Current value: {value}, Recommended: {recommended_versions[header]}")
+    if( parser.compare_versions or parser.verbose ):
+        outdated_headers = check_security_header_versions(headers, parser)
+        if outdated_headers:
+            print("\n[!] The following headers are outdated:")
+            for header, value in outdated_headers.items():
+                CapHeader = "-".join( [ word.capitalize() for word in header.split("-") ] )
+                print(f"    - {CapHeader}:")
+                print(f"        Current value: {value}")
+                print(f"        Recommended:   {recommended_versions[header.lower()]}")
     if parser.verbose:
         verbose()
     elif parser.target:
